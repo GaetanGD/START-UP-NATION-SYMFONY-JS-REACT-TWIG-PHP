b54692d122049d2bb201a78e2fe6e7bacb710c38
gaetangd <92740722+GaetanGD@users.noreply.github.com>
2021-12-13 16:51:02 +0100

    register ajout de newsletter name, lastname, cgu

--- MAYOR/src/Controller/RegistrationController.php
+++ MAYOR/src/Controller/RegistrationController.php
@@ -30,7 +30,8 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+            $date = new \DateTime();
+            $user -> setCgu($date);
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
