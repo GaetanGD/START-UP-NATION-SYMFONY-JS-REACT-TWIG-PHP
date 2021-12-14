151995583d944edb4f291704d689cd7a257ca075
gaetangd <92740722+GaetanGD@users.noreply.github.com>
2021-12-14 16:20:07 +0100

    restaurant form

---
+++ MAYOR/src/Controller/RestaurantFormController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Activity;
+use App\Form\RestaurantFormType;
+use Doctrine\ORM\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RestaurantFormController extends AbstractController
+{
+    /**
+     * @Route("/restaurant/form", name="restaurant_form")
+     */
+    public function index(): Response
+    {
+        $restaurant = new Restaurant();
+        $form = $this->createForm(RestaurantFormType::class, $restaurant);
+
+        return $this->render('restaurant_form/index.html.twig', [
+            'controller_name' => 'RestaurantFormController',
+
+            'RestaurantForm' =>  $form->createView()
+        ]);
+    }
+}
---
+++ MAYOR/src/Form/RestaurantFormType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Activity;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RestaurantFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('main_picture')
+            ->add('price')
+            ->add('equipment')
+            ->add('reservation')
+            ->add('atmosphere')
+            ->add('country')
+            ->add('postal_code')
+            ->add('state')
+            ->add('city')
+            ->add('address_1')
+            ->add('address_2')
+            ->add('description')
+            ->add('strong_point')
+            ->add('weak_point')
+            ->add('calendar')
+            ->add('yes')
+            ->add('user_id')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Activity::class,
+        ]);
+    }
+}
