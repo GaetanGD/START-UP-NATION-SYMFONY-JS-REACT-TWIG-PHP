0eb84e4e68aa513dfad2f59531dadae715a71264
gaetangd <92740722+GaetanGD@users.noreply.github.com>
2021-12-20 13:01:13 +0100

    css

--- MAYOR/src/Controller/HomeController.php
+++ MAYOR/src/Controller/HomeController.php
@@ -15,6 +15,9 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'navbartitle' => true,
+            'searchnavbar' => true,
+            'nav' => true
         ]);
     }
 }
