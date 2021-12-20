7d2c15eb8f167259d7ca03bd9c218e4ce2e12f2e
gaetangd <92740722+GaetanGD@users.noreply.github.com>
2021-12-20 09:41:27 +0100

    cours react

--- MAYOR/assets/components/Recherche.js
+++ MAYOR/assets/components/Recherche.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState, useCallback, useEffect, useRef } from "react";
 
 const Recherche = () => {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  let timerRef = useRef();
 
+  const callApi = useCallback(() => {
+    console.log("Call API");
+    fetch(`/api/auteurs/search/${query}`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("DATA", data);
+            setResults(data);
+        });
+  }, [query]);
 
-    return(
-        <input type="text" />
-    )
-}
+  useEffect(() => {
+    if (query.length > 0) {
+      clearTimeout(timerRef.current);
+      timerRef.current = setTimeout(() => {
+        callApi();
+      }, 1000);
+    }
+  }, [query, callApi]);
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+  return (
+      <>
+        <input type="text" onChange={handleChange} value={query} />
+        <ul>
+            {results.map((auteur) => {
+                return(
+                    <li key={auteur.id}>{auteur.name}</li>
+                )
+            })}
+        </ul>
+    </>
+    
+  )
+};
 
 export default Recherche;
