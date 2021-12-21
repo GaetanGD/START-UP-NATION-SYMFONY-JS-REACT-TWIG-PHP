3cd5c9135343df9c614d6bec1510999bcdb394e9
AdrienPC <adrien_paris@yahoo.fr>
2021-12-21 15:11:29 +0100

    fomulaire de voayge

--- MAYOR/src/Controller/VoyageFormController.php
+++ MAYOR/src/Controller/VoyageFormController.php
@@ -4,20 +4,44 @@ namespace App\Controller;
 
 use App\Form\VoyageFormType;
 use App\Entity\Travel;
-use Doctrine\ORM\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class VoyageFormController extends AbstractController
 {
     /**
      * @Route("/voyage/form", name="voyage_form")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $travel = new Travel();
         $form = $this->createForm(VoyageFormType::class, $travel);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $someNewFilename = uniqid(); 
+            
+            $directory = '/public/uploads';
+
+            $file = $form['main_picture']->getData();
+            $extension = $file->guessExtension();
+    
+            if (!$extension) {
+                $extension = 'bin';
+            }
+            $file->move($directory, $someNewFilename.'.'.$extension);
+
+            $travel->setMainPicture($someNewFilename.'.'.$extension);
+
+            $em =$this->getDoctrine()->getManager();
+            $em->persist($travel);
+            $em->flush();
+    
+        }
 
         return $this->render('voyage_form/index.html.twig', [
             'controller_name' => 'VoyageFormController',
@@ -25,4 +49,3 @@ class VoyageFormController extends AbstractController
         ]);
     }
 }
-
--- MAYOR/src/Form/VoyageFormType.php
+++ MAYOR/src/Form/VoyageFormType.php
@@ -7,20 +7,66 @@ use App\Entity\Travel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\RangeType;
+use Symfony\Component\Validator\Constraints\File;
 
 class VoyageFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('main_picture')
+        ->add('name', TextareaType::class, [
+            'attr' => ['class' => 'tinymce'],
+        ])
+        ->add('main_picture', FileType::class, [
+            'label' => 'Photo de couverture',
+            'mapped' => false,
+            'required' => false,
+            'constraints' => [
+                new File([
+                    'maxSize' => '4024k',
+                    'mimeTypes' => [
+                        'imagee/jpeg', 
+                        'imagee/jpg', 
+                        'imagee/png'
+                    ],
+                    'mimeTypesMessage' => 'Votre photo est non valide',
+                ])
+            ],
+        ])
            # ->add('activity')
            # ->add('user_id')
-            ->add('transport')
-            ->add('weather_report')
+            ->add('transport', ChoiceType::class, [
+                'choices' => [
+                    'avion' => 1,
+                    'train' => 2,
+                    'bus' => 3,
+                    'voiture' => 4,
+                    'moto' => 5,
+                    'vélo' => 6,
+                    'marche' => 7,
+                ],
+                'expanded'=> true,
+                'multiple'=> true,
+            ])
+            ->add('weather_report', RangeType::class, [
+                'attr' => [
+                    'min' => 1,
+                    'max' => 4,
+                    'class' => 'range_form'
+                ]
+            ])
             ->add('price')
-            ->add('equipment')
+            ->add('equipment', ChoiceType::class, [
+                'choices' => [
+                    'oui' => 'oui',
+                    'non' => 'non',
+                ],
+            'expanded' => true
+            ])
             ->add('description')
             ->add('advice')
         ;
